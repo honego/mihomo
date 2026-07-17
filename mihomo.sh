2c38da7a131f4eeff5569f11c3268606a40bb6a3
havario <157877551+havario@users.noreply.github.com>
2026-07-18 02:15:03 +0800

    -
    Signed-off-by: havario <157877551+havario@users.noreply.github.com>

--- mihomo.sh
+++ mihomo.sh
@@ -4,7 +4,7 @@
 
 # shellcheck disable=SC1091,SC2034
 
-readonly SCRIPT_VERSION="1.0.0"
+readonly SCRIPT_VERSION='1.0.0'
 
 ARGS=("$@")
 . /etc/mihomo/sh/init.sh
---
+++ sh/init.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+# SPDX-License-Identifier: Apache-2.0
+# Copyright (c) 2026 honeok <i@honeok.com>
